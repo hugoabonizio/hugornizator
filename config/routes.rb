a64417b1c2255383ce988302aafcd8851d4e9a23
hugoabonizio <hugo_abonizio@hotmail.com>
2015-07-03 17:08:32 +0000

    Config devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'list/index'
-
   devise_for :users
-  root 'lists#index'
+  root 'list#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
