066f2dacb44c41e72c3b7cc71d525e312f9cc752
hugoabonizio <hugo_abonizio@hotmail.com>
2015-07-03 20:47:38 +0000

    Creating lists

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -4,4 +4,11 @@ class ListController < ApplicationController
   def index
     @lists = current_user.lists
   end
+  
+  def create
+    @list = List.new(params.require(:list).permit(:name).merge(user_id: current_user.id))
+    if @list.save
+      redirect_to root_path
+    end
+  end
 end
