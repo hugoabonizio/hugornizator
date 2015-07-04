8bce3c9a4fc68d6f357decf2e3e5ac362b486a5e
hugoabonizio <hugo_abonizio@hotmail.com>
2015-07-04 18:44:46 +0000

    Referential integrity in lists and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  has_many :lists
+  has_many :lists, dependent: :delete_all
 end
